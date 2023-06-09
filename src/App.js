bbdc32ad66a047fccaf8d04f272ef1ee8475b6fe
tuan0101 <tuan.programmer0101@gmail.com>
2023-06-09 00:09:41 -0700

    change white bg

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
   const slides = [
-     "assets/photos/slide2.jpg",
-     "assets/photos/slide3.jpg",
-     "assets/photos/slide4.jpg",
-     "assets/photos/slide5.jpg",
-     "assets/photos/slide6.jpg",
-     "assets/photos/slide7.jpg",
-     "assets/photos/slide8.jpg",
+    "assets/photos/slide2.jpg",
+    "assets/photos/slide3.jpg",
+    "assets/photos/slide4.jpg",
+    "assets/photos/slide5.jpg",
+    "assets/photos/slide6.jpg",
+    "assets/photos/slide7.jpg",
+    "assets/photos/slide8.jpg",
   ]
 
   useEffect(() => {
@@ -46,90 +46,93 @@ function App() {
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar 
+      <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage} setSelectedPage={setSelectedPage}
       />
       <Landing setSelectedPage={setSelectedPage} />
 
       <LineGradient />
-      <div  className="pb-4">
-        <Youtube 
+      <div className="pb-4">
+        <Youtube
           selectedPage={selectedPage} setSelectedPage={setSelectedPage}
         />
       </div>
 
-      <div className="text-5xl font-playfair font-semibold text-red z-10 text-center py-5">
-        About Us
-      </div>
-      <div className="w-5/6 mx-auto xs:mt-2">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("ourstaff")}
-        >
-          <AboutStaff setSelectedPage={setSelectedPage}/>
-        </motion.div>
+      <div className="bg-white">
+        <div className="text-5xl font-playfair font-semibold text-red z-10 text-center py-5 ">
+          About Us
+        </div>
+        <div className="w-5/6 mx-auto xs:mt-2">
+          <motion.div
+            margin="0 0 -200px 0"
+            amount="all"
+            onViewportEnter={() => setSelectedPage("ourstaff")}
+          >
+            <AboutStaff setSelectedPage={setSelectedPage} />
+          </motion.div>
+        </div>
+
+        <div className="w-[90%] mx-auto  m-10">
+          <motion.div
+            margin="0 0 -200px 0"
+            amount="all"
+            onViewportEnter={() => setSelectedPage("ourhistory")}
+          >
+            <AboutHistory setSelectedPage={setSelectedPage} />
+          </motion.div>
+        </div>
+
+        <div className="w-5/6 mx-auto pb-10">
+          <motion.div
+            margin="0 0 -200px 0"
+            amount="all"
+            onViewportEnter={() => setSelectedPage("ourmission")}
+          >
+            <AboutMission setSelectedPage={setSelectedPage} />
+          </motion.div>
+        </div>
       </div>
 
-      <div className="w-[90%] mx-auto  m-10">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("ourhistory")}
-        >
-          <AboutHistory setSelectedPage={setSelectedPage}/>
-        </motion.div>
-      </div>
-      
-      <div className="w-5/6 mx-auto mb-10">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage("ourmission")}
-        >
-          <AboutMission setSelectedPage={setSelectedPage}/>
-        </motion.div>
-      </div>
 
       <LineGradient />
       <div className="py-10 w-[70%]  mx-auto" >
         <Carousel>
           {[
             ...slides.map((s) =>
-            <img src={s} alt="tony"/>)
+              <img src={s} alt="tony" />)
           ]}
         </Carousel>
       </div>
 
       <LineGradient />
-      <div  className="w-[90%] mx-auto xs:mt-2">
-        <History 
+      <div className="w-[90%] mx-auto xs:mt-2">
+        <History
           selectedPage={selectedPage} setSelectedPage={setSelectedPage}
         />
       </div>
 
 
-      <LineGradient />    
+      <LineGradient />
+      <div className="bg-white">
+        <Mission
+          selectedPage={selectedPage} setSelectedPage={setSelectedPage}
+        />
+      </div>
+
+      <LineGradient />
       <div className="">
-      <Mission 
+        <Staff
           selectedPage={selectedPage} setSelectedPage={setSelectedPage}
         />
       </div>
 
-      <LineGradient />    
-      <div className="">
-      <Staff 
-          selectedPage={selectedPage} setSelectedPage={setSelectedPage}
-        />
-      </div>
-
-      <LineGradient />  
+      <LineGradient />
       <Donation setSelectedPage={setSelectedPage} />
 
       <LineGradient />
-      <div  className="my-10 pb-12">
-        <Contact 
+      <div className="my-10 pb-12">
+        <Contact
           selectedPage={selectedPage} setSelectedPage={setSelectedPage}
         />
       </div>
